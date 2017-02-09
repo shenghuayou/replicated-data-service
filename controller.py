a1d2a3b0a44457d15ac47beb4830f5acd386b22c
VictorFung1 <victor.fung122@gmail.com>
2017-02-08 21:44:08 -0500

    changed the flow of op

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@
 import select
 import socket
 import sys
-import pymysql
+#import pymysql
 from random import randint
 
 host = 'localhost' # what address is the server listening on
@@ -33,34 +33,40 @@ def foward_to_server(data_from_client, client_to_controller, controller_to_serve
     client_to_controller.send(return_statement.encode('utf-8'))
 
 running = 1 #set running to zero to close the server
+server_list = []
 print('Controller is up and awaiting connections! \n')
 while running:
   inputready,outputready,exceptready = select.select(input,[],[])
 
   for client_to_controller in inputready: #check each socket that select() said has available data
-
     if client_to_controller == controller: #if select returns our server socket, there is a new remote socket trying to connect
       client, address = controller.accept()
       input.append(client) #add it to the socket list so we can check it now
-      print ('New connection with controller added - id is %s'%str(address))
-
+      print ('New connection with controller added - id is %s' % str(address))
     else:
       # select has indicated that these sockets have data available to recv
       data_from_client = client_to_controller.recv(BUFFER_SIZE) # Between client and controller - foward response to server
       if data_from_client:
-        turn = randint(1,2)
-        controller_to_server_one = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result_s1 = controller_to_server_one.connect_ex((host, server_port_one))
-        controller_to_server_two = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result_s2 = controller_to_server_two.connect_ex((host, server_port_two))
-        if turn == 1:
-          foward_to_server(data_from_client, client_to_controller, controller_to_server_one, result_s1)
-        if turn == 2:
-          foward_to_server(data_from_client, client_to_controller, controller_to_server_two, result_s2)
+        if ':' in str(data_from_client):
+            server_port = str(data_from_client).split(':')[-1]
+            server_port = server_port[:-1]
+            print('Server identified - the port is %s' % server_port)
+            server_list.append(int(server_port))
+            print("Server list => %s " % (server_list))
+        else:
+            #turn = randint(1,2)
+            print("Server list length => %s " % (len(server_list)))
+            if len(server_list) <= 0:
+                return_statement = 'All servers are down, unable to process request.'
+                client_to_controller.send(return_statement.encode('utf-8'))
+            else:
+                turn = randint(0,len(server_list)-1)
+                print('The selected server (port) is %s out of the %s number of avaliable servers' % (str(server_list[turn]), len(server_list)))
+                controller_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # This will serve as a ping request
+                result = controller_to_server.connect_ex((host, int(server_list[turn])))
+                foward_to_server(data_from_client, client_to_controller, controller_to_server, result)
 
-
-      else: #if recv() returned NULL, that usually means the sender wants
-            #to close the socket.
+      else: #if recv() returned NULL, that usually means the sender wants to close the socket.
         print('Action complete - closing connection %s with server.' % (str(address)))
         client_to_controller.close()
         input.remove(client_to_controller)
--- server.py
+++ server.py
@@ -3,24 +3,22 @@
 import select
 import socket
 import sys
-import pymysql
+#import pymysql
 
-def checkmoney():
-  db = pymysql.connect("seniordesign.c9btkcvedeon.us-west-2.rds.amazonaws.com","root","qwe123456","senior_design" )
-  cursor = db.cursor()
-  cursor.execute("use senior_design;")
-  cursor.execute("select * from property where name='client1';")
-  result = cursor.fetchall()
-  db.close()
-  return (result[0])
+#def checkmoney():
+#  db = pymysql.connect("seniordesign.c9btkcvedeon.us-west-2.rds.amazonaws.com","root","qwe123456","senior_design" )
+#  cursor = db.cursor()
+#  cursor.execute("use senior_design;")
+#  cursor.execute("select * from property where name='client1';")
+#  result = cursor.fetchall()
+#  db.close()
+#  return (result[0])
 
 
 host = 'localhost' # what address is the server listening on
 port = 9997 # what port the server accepts connections on
 backlog = 5  # how many connections to accept
 BUFFER_SIZE = 1024 # Max receive buffer size, in bytes, per recv() call
-
-#now initialize the server and accept connections at localhost:50000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
@@ -29,38 +27,40 @@ input = [server,] #a list of all connections we want to check for data
                   #each time we call select.select()
 
 running = 1 #set running to zero to close the server
-print('Server is up and awaiting connections! \n')
-while running:
-  inputready,outputready,exceptready = select.select(input,[],[])
-
-  for s in inputready: #check each socket that select() said has available data
-
-    if s == server: #if select returns our server socket, there is a new
-                    #remote socket trying to connect
-      client, address = server.accept()
-      input.append(client) #add it to the socket list so we can check it now
-      print ('New connection with controller added - id is %s'%str(address))
-
-    else:
-      # select has indicated that these sockets have data available to recv
-      data = s.recv(BUFFER_SIZE)
-      if data:
-        #execute database queries here-----------------------------------------------------
-        if data.decode("utf-8")=='checkmoney':
-          result=checkmoney()
-          s.send(str(result).encode('utf-8'))
-
+print('Server is up and connecting to the controller! \n')
+controller_port = 9996
+server_to_controller = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection_result = server_to_controller.connect_ex((host, controller_port))
+if connection_result == 0:
+    print('Successful connection to the controller')
+    identify_as_server = str('0x4920616d206120736572766572:')+str(port)
+    server_to_controller.send(identify_as_server.encode('utf-8')) # foward string to identify as server to the controller
+    while running:
+      inputready,outputready,exceptready = select.select(input,[],[])
+      for s in inputready: #check each socket that select() said has available data
+        if s == server: #if select returns our server socket, there is a new remote socket trying to connect
+          client, address = server.accept()
+          input.append(client) #add it to the socket list so we can check it now
+          print ('New connection with server added - id is %s'%str(address))
         else:
-          print ('%s received from %s'%(data,s.getsockname()))
-          #Uncomment below to echo the recv'd data back
-          #to the sender... loopback!
-          return_statement = 'Successful foward from controller .'
-          s.send(return_statement.encode('utf-8'))
-      else: #if recv() returned NULL, that usually means the sender wants
-            #to close the socket.
-        print('Action complete - closing connection %s with controller.' % (str(address)))
-        s.close()
-        input.remove(s)
+          # select has indicated that these sockets have data available to recv
+          data = s.recv(BUFFER_SIZE)
+          if data:
+            #execute database queries here-----------------------------------------------------
+            if data.decode("utf-8")=='checkmoney':
+              #result = checkmoney()
+              s.send(str(result).encode('utf-8'))
+            else:
+              print ('%s received from %s'%(data,s.getsockname()))
+              return_statement = 'Successful foward from controller .'
+              s.send(return_statement.encode('utf-8'))
+          else: # close the socket (connection)
+            print('Action complete - closing connection %s with controller.' % (str(address)))
+            s.close()
+            input.remove(s)
+else:
+    print('Failed to connect to the controller')
+    server.close()
 
 #if running is ever set to zero, we will call this
 server.close()
