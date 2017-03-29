cd5812b72116ac2d76d0dc4da9e82ba0eeda03b4
Victor Fung <victor.fung122@gmail.com>
2017-03-29 17:01:21 -0400

    added the creation of .ini file, need to create csv files for data/* now

--- controller.py
+++ controller.py
@@ -4,7 +4,14 @@ import select
 import socket
 import sys
 #import pymysql
+import csv
+import os
+import glob
+import datetime
+import ConfigParser
+import threading
 from random import randint
+
 
 host = 'localhost' # what address is the server listening on
 port = 9996 # what port the server accepts connections on
@@ -23,11 +30,14 @@ input = [controller,] #a list of all connections we want to check for data
 
 #definition here-----------------------------------------------------
 def foward_to_server(data_from_client, client_to_controller, controller_to_server, result):
-  # Connect controller to ser{ver
+  # Connect controller to server
   if result == 0: # server is alive if result = 0
+    # record the serverid and current datetime (controller->server)
     controller_to_server.send(data_from_client) # foward data from client to servers
     data_from_server = controller_to_server.recv(BUFFER_SIZE) # recieve feedback from the server
+    # append on the current datetime (server->controller), record in csv_index.csv
     client_to_controller.send(data_from_server) # foward back to client
+
   else: # server is dead
     return_statement = 'Server is down, unable to process request.'
     client_to_controller.send(return_statement.encode('utf-8'))
@@ -43,6 +53,42 @@ def ping_servers(servers, connect_status):
       index = servers.index(server) # get the index of the disconnected server
       connect_status.pop(index) # remove it from the connect_status list
       servers.remove(server) # remove port from server list (server disconnected)
+
+def updateIndex():
+    config = ConfigParser.ConfigParser()
+    config.read('info.ini') # open the file
+    ci = config.get('INDEX', 'CurrentIndex') # get the current value
+    ci = int(ci) + 1
+    config.set('INDEX','CurrentIndex', str(ci)) # set the update value
+    with open('info.ini', 'w') as configfile:
+        config.write(configfile) # save the updated value(s)
+    threading.Timer(60, updateIndex).start() # start every 60 seconds
+
+# Create the data folder, will need to clear all contents
+# of data folder at restart of controller
+try:
+    os.makedirs('data')
+except OSError:
+    pass
+
+files = [file for file in glob.glob("data/*.csv")]
+for file in files:
+    os.remove(file)
+
+# Create the info.dat file which will be used later for
+# determining server order and make it empty
+info_file = open('info.ini','w+')
+info_file.close()
+config = ConfigParser.ConfigParser()
+config.read('info.ini')
+config.add_section('SERVER')
+config.add_section('INDEX')
+config.set('INDEX', 'CurrentIndex', 0)
+with open('info.ini', 'w') as configfile:
+    config.write(configfile)
+
+# start calling f now and every 60 sec thereafter
+updateIndex()
 
 running = 1 #set running to zero to close the server
 server_list = []
@@ -64,7 +110,7 @@ while running:
       if data_from_client:
         if '0x4920616d206120736572766572:' in str(data_from_client):
             server_port = str(data_from_client).split(':')[-1]
-            server_port = server_port[:-1] # For some reason on AmazonEC2, we need to remove the [:-1]
+            server_port = server_port # For some reason on AmazonEC2, we need to remove the [:-1]
             print('Server identified - the port is %s' % (str(server_port)))
             server_list.append(int(server_port))
             already_connected.append(False)
@@ -93,7 +139,7 @@ while running:
 
                 #requests are randomly distributed to servers
                 # turn = randint(0,len(server_list)-1) # determine which server to direct to
-                # if already_connected[turn] == False: # reconnect 
+                # if already_connected[turn] == False: # reconnect
                 #     print('The selected server (port) is %s out of the %s number of avaliable servers' % (str(server_list[turn]), len(server_list)))
                 #     controller_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # This will serve as a ping request
                 #     result = controller_to_server.connect_ex((host, int(server_list[turn])))
