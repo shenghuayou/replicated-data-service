dd2e8e5f5e49418e8dda56b4c96fe1481f0c4a6d
shenghuayou <syou000@citymail.cuny.edu>
2017-05-19 14:40:07 -0400

    clean files

--- runrep.sh
+++ runrep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python controller.py"'
-osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python server.py 9999"'
-osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python server.py 9998"'
-osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python bot.py sheng addmoney100"'
-osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python bot.py sheng checkmoney"'
+osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python local_controller.py"'
+osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python local_server.py 9997"'
+osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python local_server.py 9998"'
+osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python local_bot.py sheng"'
+osascript -e 'tell application "Terminal" to do script "cd Desktop/rep* && python local_bot.py client1"'
